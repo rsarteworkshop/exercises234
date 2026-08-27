4f85ec699dfe8b3627941174e216a27e9639655d
Mattias Mohlin <mattias.mohlin@hcl.com>
2026-08-27 15:46:29 +0200

    First version of web ui

---
+++ WebUI/app.js
@@ -0,0 +1,99 @@
+/*******************************************************************************
+ * (c) Copyright HCL Technologies Ltd. 2018.  MIT Licensed!
+ *******************************************************************************/
+
+/**
+ * Server application entry point
+ * @author Mattias Mohlin
+ */
+'use strict';
+
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+const port = 7004;
+const env = process.env.NODE_ENV || 'development';
+
+
+// Static middleware for serving static files 
+app.get('/', function(req, res) {
+    res.contentType("text/html");
+    res.sendFile(__dirname + '/public/html/main.html');
+});
+app.get('/css', function(req, res) {
+    res.contentType("text/css");
+    res.sendFile(__dirname + '/public/css/styling.css');
+});
+app.get('/images/stop', function(req, res) {
+    res.contentType("img/png");
+    res.sendFile(__dirname + '/public/images/stop.png');
+});
+app.get('/images/walk', function(req, res) {
+    res.contentType("img/png");
+    res.sendFile(__dirname + '/public/images/walk.png');
+});
+app.get('/main', function(req, res) {
+    res.contentType("text/javascript");
+    res.sendFile(__dirname + '/public/js/main.js');
+});
+app.get('/jquery', function(req, res) {
+    res.contentType("text/javascript");
+    res.sendFile(__dirname + '/public/js/jquery/jquery.min.js');
+});
+
+// Called when a message is received from the RT application
+function msgReceived(msg) {
+    if (msg.port == "lightChange") {        
+
+        if (msg.event == "lightChanged") {
+            // Expected format: LightChangeData{color"<color>",name"<name>",index <index>}
+            const match = /^LightChangeData\{color"([^"]+)",name"([^"]+)",index\s+(\d+)\}$/.exec(msg.data);
+
+            if (match) {
+                const color = match[1];
+                const name = match[2];
+                const index = parseInt(match[3], 10);
+
+                io.emit('light', {
+                    'light' : color,
+                    'name' : name,
+                    'index' : index
+                });
+            }
+            else {
+                console.warn('Invalid lightChanged payload:', msg.data);
+            }
+        }
+    }
+    else if (msg.port == "pedLightControl") {
+        // Message from pedestrian light
+        if (msg.event == "stop") {
+            io.emit('pedlight', {'light' : 'stop'});
+        }
+        else if (msg.event == "walk") {
+            io.emit('pedlight', {'light' : 'walk'});            
+        }
+        else if (msg.event == "timeRemaining") {
+            io.emit('pedcount', {'count' : msg.data.split(" ")[1]}); // Strip off data type and only keep the value
+        }
+    }
+}
+
+const tcpServer = require('rt-tcp-utils')('localhost', 7001); // Send TCP requests to RT app on port 7001
+
+tcpServer.setEventReceivedCallback(msgReceived);
+tcpServer.startListenForEvents(7002) // Receive TCP requests from RT app on port 7002
+.then((data) => {
+    console.log("Ready to receive TCP requests");    
+});
+
+// Messages from web application to RT application 
+app.get('/ped_button', function(req, res) {
+    tcpServer.sendEvent('pedestrian', 'trafficLight');
+
+    res.end();
+});
+
+http.listen(port, () => console.log(`Web app listening on port ${port}!`));
---
+++ WebUI/public/js/main.js
@@ -0,0 +1,75 @@
+/*******************************************************************************
+ * (c) Copyright HCL Technologies Ltd. 2018.  MIT Licensed!
+ *******************************************************************************/
+
+/**
+ * Client application entry point
+ * @author Mattias Mohlin
+ */
+
+$(function () {
+
+    var socket = undefined;
+    var isBrowser = true;
+    
+    if (isBrowser) {
+        const baseUrl = `${window.location.protocol}//${window.location.hostname}${window.location.port ? `:${window.location.port}` : ''}`;
+        const socketPath = `${window.location.pathname}socket.io`;
+        console.log("baseUrl: " + baseUrl);
+        console.log("socketPath: " + socketPath);
+        socket = io(baseUrl,{
+            path: socketPath
+          });
+    
+    } else {
+        socket = io();
+    }
+
+   
+    socket.on('light', function(msg) {
+        $('#light-label').text("Light: " + msg.light);
+        $('#rl').css('fill', 'black');
+        $('#yl').css('fill', 'black');
+        $('#gl').css('fill', 'black');
+        if (msg.light == 'Red') {
+            $('#rl').css('fill', 'red');            
+        }
+        else if (msg.light == 'Yellow') {
+            $('#yl').css('fill', 'yellow');
+        }
+        else if (msg.light == 'Green') {
+            $('#gl').css('fill', 'green');
+        }
+    });
+    
+    socket.on('pedlight', function(msg) {
+
+        if(isBrowser){
+            $('#pedSignal').attr('src', './images/' + msg.light);
+        }else{
+            $('#pedSignal').attr('src', '/images/' + msg.light);
+        }
+        
+    });
+
+    socket.on('pedcount', function(msg) {
+        $('#pedCounter').text(msg.count);
+        if (msg.count == '0') {
+            setTimeout(() => {$('#pedCounter').text('');}, 1000);
+        }
+    });
+
+    $('#ped_button').unbind('click').bind('click', function (e) {
+        if(isBrowser){
+            $.get('./ped_button', function () {
+
+            });
+        }
+        else{
+            $.get('/ped_button', function () {
+
+            });
+        }
+        
+    });
+});
